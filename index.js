8564771d54b40f9a01ef86ce9c058b8a1f9406e7
hhuong17 <115204765+hhuong17@users.noreply.github.com>
2024-09-12 17:13:07 +0700

    Express Serve Static Files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express'),
 const hostname = 'localhost';
 const port = 3000;
 const app = express();
+const morgan = require('morgan');
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
@@ -10,7 +11,8 @@ app.use((req, res, next) => {
     res.end('<html> <body><h1>This is an Express Server</h1></body></html>');
 
 });
-
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'))
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
